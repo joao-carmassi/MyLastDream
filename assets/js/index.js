3bbfd4b61bad5a6bb16b05b82d40f052598b9256
Joao Carmassi <joaovitorcarmassi@gmail.com>
2024-10-14 19:10:25 -0300

    primero commit

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+// Dooms -----
+const containerGaleria = document.querySelector(".containerGaleria");
+
+// Codigo -----
+document.addEventListener("DOMContentLoaded", async () => {
+  await adicionaFotosNaGaleria();
+});
+
+async function adicionaFotosNaGaleria() {
+  const data = await capituraFotos();
+
+  // TODO: arruma nao pode espaco em nome da regiao
+  // Gera Categorias
+  const listaDeLugares = Object.keys(data);
+  listaDeLugares.forEach((lugar) => {
+    const idRegiao = lugar.replace(" ", "-");
+    containerGaleria.innerHTML += `
+    <div id='${idRegiao}'>
+        <h2>${lugar}</h2>
+        <span id='container__${idRegiao}'>b
+        </span>
+    </div>
+    `;
+
+    // Gera imagens
+    const fotosDoLugar = data[lugar];
+    fotosDoLugar.forEach((fotos) => {
+      const containerLugar = document.getElementById(`container__${lugar}`);
+      containerLugar.innerHTML += `
+          <img class="aparesceAnimacaoFotos" src='./assets/imgs/${fotos}' />
+        `;
+    });
+  });
+}
+
+async function capituraFotos() {
+  const res = await fetch("./assets/db.json");
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
